37675aa336fab3643b1b40d13a72c85d32b68968
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-05-22 15:29:26 +0200

    Creato un pò di serie e film

--- server.js
+++ server.js
@@ -26,6 +26,24 @@ class serie extends movieSerie{
     }
 }
 
+// Creazione serie televisive
+const serie1 = new TvSerie('Breaking Bad', 2008, 'Drammatico', 4 , 'Serie', 5);
+const serie2 = new TvSerie('Stranger Things', 2016, 'Horror', 3 , 'Serie', 4);
+const serie3 = new TvSerie('The Boys', 2019, 'Drammatico', 5 , 'Serie', 4);
+const serie4 = new TvSerie('how i met your mother', 2005, 'Sitcom', 4 , 'Serie', 9);
+
+// Creazione film
+const film1 = new movieSerie('Talk to me', 2022, 'Horror', 3 , 'Film');
+const film2 = new movieSerie('V per vendetta', 2005, 'Azione', 5 , 'Film');
+const film3 = new movieSerie('Joker', 2019, 'Thriller', 4 , 'Film');
+const film4 = new movieSerie('Sinister', 2012, 'Horror', 3 , 'Film');
+
+
+
+
+
+
+
 
 http
     .createServer(function(req, res){
