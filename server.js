a213ed81d6143d59397ec0392dc3113a6ecca997
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-05-22 17:13:34 +0200

    Diviso array per type

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ class movieSerie{
         this.rating = rating;
         this.type = type;
     }
+
+    toString(){
+        return `${this.title} è un film di genere ${this.genre} rilasciato nel ${this.year} che ha come valutazione ${this.rating}/5`;
+    };
 }
 
 // Estensione Serie
@@ -24,20 +28,47 @@ class serie extends movieSerie{
         super(title, year, genre, rating, type);
         this.seasons = seasons;
     }
+    toString(){
+        return `${this.title} è una serie di genere ${this.genre} rilasciato nel ${this.year} che ha ${this.seasons} stagioni ed ha una valutazione di ${this.rating}/5`  ;
+    };
 }
 
-// Creazione serie televisive
-const serie1 = new TvSerie('Breaking Bad', 2008, 'Drammatico', 4 , 'Serie', 5);
-const serie2 = new TvSerie('Stranger Things', 2016, 'Horror', 3 , 'Serie', 4);
-const serie3 = new TvSerie('The Boys', 2019, 'Drammatico', 5 , 'Serie', 4);
-const serie4 = new TvSerie('how i met your mother', 2005, 'Sitcom', 4 , 'Serie', 9);
+// Creazione film e Serie
+const filmSerie = [
+        // Creazione serie televisive
+        serie1 = new serie('Breaking Bad', 2008, 'Drammatico', 4 , 'Serie', 5),
+        serie2 = new serie('Stranger Things', 2016, 'Horror', 3 , 'Serie', 4),
+        serie3 = new serie('The Boys', 2019, 'Drammatico', 5 , 'Serie', 4),
+        serie4 = new serie('how i met your mother', 2005, 'Sitcom', 4 , 'Serie', 9),
 
-// Creazione film
-const film1 = new movieSerie('Talk to me', 2022, 'Horror', 3 , 'Film');
-const film2 = new movieSerie('V per vendetta', 2005, 'Azione', 5 , 'Film');
-const film3 = new movieSerie('Joker', 2019, 'Thriller', 4 , 'Film');
-const film4 = new movieSerie('Sinister', 2012, 'Horror', 3 , 'Film');
+        // Creazione film
+        film1 = new movieSerie('Talk to me', 2022, 'Horror', 3 , 'Film'),
+        film2 = new movieSerie('V per vendetta', 2005, 'Azione', 5 , 'Film'),
+        film3 = new movieSerie('Joker', 2019, 'Thriller', 4 , 'Film'),
+        film4 = new movieSerie('Sinister', 2012, 'Horror', 3 , 'Film'),
+]
 
+// Filtrare per type e inserire in un array di nome film
+const film = filmSerie.map(item => {
+    let films = []
+    if (item.type == 'Film'){
+        films.push(item)
+    }
+    return films
+})
+
+// Filtrare per type e inserire in un array di nome serie
+const serieTV = filmSerie.map(item => {
+    let series = []
+    if (item.type == 'Serie'){
+        series.push(item)
+    }
+    return series
+})
+
+
+// console.log(`${film}`)
+// console.log(`${serieTV}`)
 
 
 
@@ -51,7 +82,8 @@ http
             "content-Type":"text/html; charset=utf-8"
         });
         res.end(
-            `<h1>Server on</h1>`
+            `<h1>${film1}</h1>
+             <h1>${serie1}</h1>`
         );
     })
     .listen(port, host, () => {
